ecb73cff527c2114a8773fd3a871c577a6f6463e
swaraj <sswaraj169@gmail.com>
2023-06-30 23:55:42 +0530

    updated flutter_approuter.dart

--- lib/flutter_approuter.dart
+++ lib/flutter_approuter.dart
@@ -1,3 +1,7 @@
+/// Flutter Navigation made easy, now navigate to screens, without BuildContext from pure flutter & dart code.
+///
+/// Get started at [pub.dev](https://pub.dev/packages/flutter_approuter) 🚀
+
 library flutter_routerx;
 
 export 'src/approuter/approuter.dart';
